b4d867fdcc420a4956b6ab267c9ab40eb61d9e3b
MujeebAdelekan <56655073+MujeebAdelekan@users.noreply.github.com>
2022-11-08 19:49:24 -0600

    Created a class for the CLA (Central Legitimization Agency) Created a txt file with voter names and validation numbers Applied algorithm in Virtual Election Booth with multiple voters able to sign up in one session QUESTION: How to prevent one voter from getting multiple validation numbers

--- Virtual Election Booth/Virtual_Election_Booth.py
+++ Virtual Election Booth/Virtual_Election_Booth.py
@@ -1,2 +1,44 @@
-print('Hello World!')
+# Import CLA file
+# FIXME: How to prevent one voter from getting multiple validation numbers
+from CLA import CLA
+
+# Initialize isPollOpen to True
+isPollOpen = True
+
+while isPollOpen:
+    # Ask the voter for name
+    print("What is your name? ")
+    name = input()
+    cla = CLA(name)
+
+    # Generates and outputs validation number
+    validID = cla.generate_validation_num()
+    print(f"{cla.name}, your validation number is: {validID}. Make sure to remember it.")
+
+    # Write Name and Validation number on row in .txt file
+    #with open('voterfile.txt', encoding='utf-8') as f:
+    #    f.write("This is a test")
+    f = open('voterfile.txt', 'a+', encoding="utf-8")
+    f.write(f"{cla.name}, {validID}" + "\n")
+
+    # Ask for more votes
+    print("Are there any more voters? (y/n)")
+    answer = input()
+    if answer == 'n':
+        isPollOpen = False
+
+f.close()
+
+
+
+
+if f.closed:
+    print("voterfile is closed")
+else:
+    print("voterfile is still open")
+
+
+
+
+
 
